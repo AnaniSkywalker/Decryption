58ba86c45ebf03ebba4a397be47b84d6355c0281
AnaniSkywalker <Artificial_Intelligence@Michael-of-God.local>
2016-07-27 13:57:02 -0500

    Author

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,7 @@
+/*Author: Anani Assoutovi
+ * 
+ * NOTE: You are free to use this solution under the condition of giving credit to the "Author".
+*/
 import static java.lang.System.out;
 import java.util.Scanner;
 
