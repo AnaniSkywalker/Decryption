eb6c9c590872fd3f6eb92c1aac1ae74840aeca56
AnaniSkywalker <Artificial_Intelligence@Michael-of-God.local>
2016-07-27 13:53:51 -0500

    Solution(long x)

---
+++ src/Solution.java
@@ -0,0 +1,51 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class Solution {
+	
+	public long Solution(long x)
+	{
+		if(x >= 11 && x <= 1000000000)
+		{
+			String awe = String.valueOf(x);
+			StringBuffer stack = new StringBuffer(awe);
+			long highest = 0;
+			
+			for(int i  =0; i < stack.length(); i++)
+			{
+				if(i >0 && (stack.charAt(i) == stack.charAt(i-1)))
+				{
+					stack.deleteCharAt(i);
+					
+					long calculate = Long.parseLong(stack.toString());
+					highest = (calculate > highest)? calculate: highest;
+					
+					stack.delete(0, stack.length());
+					stack.append(awe);
+				}
+			}
+			
+			return highest > 0? highest : x;
+		}
+		else
+		{
+			throw new IllegalArgumentException("The given value must be in range of 11 to 1000000000");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Solution solver = new Solution();
+		
+		Scanner intake = new Scanner(System.in);
+		int inputVal = 0;
+		out.println("What is the value you would like to enter in: ");
+		inputVal = intake.nextInt();
+		
+		long solved = solver.Solution(inputVal);
+		out.println("The Solution is: " +solved);
+		
+		intake.close();
+	}
+
+}
